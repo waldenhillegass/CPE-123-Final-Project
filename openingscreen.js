c5a379777d838526ac2de65ed5cf1d5d081a2a68
Raphael Pham <raphaelpham14@gmail.com>
2019-12-06 16:20:49 -0800

    changed startscreen code

--- openingscreen.js
+++ openingscreen.js
@@ -1,20 +1,67 @@
-
-function setup ()
-{
-    createCanvas(800,1000);
-
-}
-
-function draw()
-{
+function setup() {
+    createCanvas(800, 1000);
+  }
+  
+  function draw() {
     background('#F3E8EE');
-    noFill();
-    stroke(73,71,64);
-    beginShape();
-        vertex(-200,0);
-        vertex(-150,
     
-
-
-}
-
+    //start button
+    fill(255);
+      quad(347, 613, 454, 613, 454, 659, 347, 659);
+    textSize(30);
+    fill(0);
+      text('Start', 366, 645);
+    
+    //mountain shadow
+    fill(0);
+    beginShape();
+      vertex(172, 460);
+      vertex(172, 460);
+      vertex(258, 242);
+      vertex(297, 326);
+      vertex(357, 176);
+      vertex(405, 276);
+      vertex(453, 161);
+      vertex(499, 325);
+      vertex(561, 251);
+      vertex(628, 459);
+      vertex(172, 460);
+      vertex(172, 460);
+    endShape();
+    //mountain
+    fill('#F3E8EE');
+    beginShape();
+      vertex(183, 451);
+      vertex(183, 451);
+      vertex(258, 260);
+      vertex(298, 345);
+      vertex(358, 193);
+      vertex(406, 294);
+      vertex(452, 184);
+      vertex(496, 341);
+      vertex(558, 266);
+      vertex(618, 451);
+      vertex(183, 451);
+      vertex(183, 451);
+    endShape();
+    //street
+    fill(0);
+      quad(198, 405, 605, 405, 624, 455, 179, 455);
+    fill(255);
+      rect(203, 430, 15, 6);
+      rect(243, 430, 15, 6);
+      rect(283, 430, 15, 6);
+      rect(323, 430, 15, 6);
+      rect(363, 430, 15, 6);
+      rect(403, 430, 15, 6);
+      rect(443, 430, 15, 6);
+      rect(483, 430, 15, 6);
+      rect(523, 430, 15, 6);
+      rect(563, 430, 15, 6);
+    
+    //title
+    textSize(70);
+    textStyle(BOLD);
+    fill('#65BB60');
+      text('CAR GAME', 214, 530);
+    }
