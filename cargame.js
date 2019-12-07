d426fec2af44295d7718e9dd2ae852ff351b4d06
sfapi1 <sfapi1@gmail.com>
2019-12-06 17:55:18 -0800

    add end screen and score stuffs

--- cargame.js
+++ cargame.js
@@ -7,6 +7,7 @@ var dashedLineY = [];
 var score = 0;
 var explosion;
 var scene = 0; // 0 - start screen    1 - game    2 - end screen
+var leaderboard; 
 
 function setup ()
 {
@@ -36,7 +37,7 @@ function draw()
             break;
         case 1:
             // game
-            background('#729B79');
+            //background('#729B79');
             drawBackground();
 
             //car(midLane(6),850,color('#AD7A99'),0); //PI=down, 0=up
@@ -55,13 +56,15 @@ function draw()
                 explosion.run();
 
                 if (explosion.allDone()) {
+                    leaderboard = getScores();
                     scene++;
+                    showNameDialog();
                 }
             }
             break;
         case 2:
             // end screen
-            background(0);
+            drawEndScreen();
             break;
     }
     
@@ -72,8 +75,171 @@ function mouseClicked() {
     if (scene == 0) {
         if (mouseX > 347 && mouseX < 454 && mouseY > 613 && mouseY < 659) {
             scene++;
+            background('#729B79');
         }
     }
+
+    if (scene == 2) {
+        if (mouseX > width/2-90 && mouseX < width/2+90 && mouseY > 57 && mouseY < 107) {
+            scene = 1;
+            explosion = undefined;
+            obstacles = [];
+            collison = false;
+            score = 0;
+            dashedLineY = [];
+
+            for(i = 0; i < obstacleCount; i++){
+                obstacles.push(new Obstacle(i));
+            }
+        
+            for (i = 0; i < height; i += 150){
+                dashedLineY.push(i);
+            }
+            background('#729B79');
+        }
+    }
+}
+
+function drawEndScreen() {
+    
+  background('#F3E8EE');
+  
+  //try again button
+  push();
+  noFill();
+  stroke('#494740');
+  strokeWeight(3);
+  rectMode(CENTER);
+    rect(width/2, 82, 180, 50);
+  textSize(30);
+  textStyle(BOLD);
+  fill('#494740');
+  noStroke();
+    textAlign(CENTER, CENTER);
+    text('Try Again?', width/2, 85);
+  pop();
+  
+  //mountain shadow
+  push();
+  fill('#494740');
+  noStroke();
+  beginShape();
+    vertex(172, 460);
+    vertex(172, 460);
+    vertex(258, 242);
+    vertex(297, 326);
+    vertex(357, 176);
+    vertex(405, 276);
+    vertex(453, 161);
+    vertex(499, 325);
+    vertex(561, 251);
+    vertex(628, 459);
+    vertex(172, 460);
+    vertex(172, 460);
+  endShape();
+  //mountain
+  fill('#F3E8EE');
+  beginShape();
+    vertex(183, 451);
+    vertex(183, 451);
+    vertex(258, 260);
+    vertex(298, 345);
+    vertex(358, 193);
+    vertex(406, 294);
+    vertex(452, 184);
+    vertex(496, 341);
+    vertex(558, 266);
+    vertex(618, 451);
+    vertex(183, 451);
+    vertex(183, 451);
+  endShape();
+  //street
+  fill('#494740');
+  noStroke();
+    quad(198, 405, 605, 405, 624, 455, 179, 455);
+  fill(255);
+    rect(203, 430, 15, 6);
+    rect(243, 430, 15, 6);
+    rect(283, 430, 15, 6);
+    rect(323, 430, 15, 6);
+    rect(363, 430, 15, 6);
+    rect(403, 430, 15, 6);
+    rect(443, 430, 15, 6);
+    rect(483, 430, 15, 6);
+    rect(523, 430, 15, 6);
+    rect(563, 430, 15, 6);
+  pop();
+  
+  //title
+  push();
+  textSize(70);
+  textStyle(BOLD);
+  textFont('Arial Black');
+  fill('#729B79');
+    textAlign(CENTER, CENTER);
+    text('YOU SUCK!', width/2, 510);
+  pop();
+  
+  //final score
+  push();
+  textSize(120);
+  textStyle(BOLD);
+  fill('#729B79');
+    textAlign(CENTER, CENTER);
+    text(score, width/2, 390);
+  pop();
+  
+  push();
+  translate(0, -50);
+  //leaderboard
+  push();
+  textSize(40);
+  textStyle(BOLD);
+  fill('#494740');
+    textAlign(CENTER, CENTER);
+    text('Leaderboard', width/2, 650);
+  pop();
+  //chart
+  push();
+  noFill();
+  stroke('#494740');
+  strokeWeight(3);
+  rectMode(CENTER);
+    rect(width/2, 700, width/2, 50);
+    rect(width/2, 750, width/2, 50);
+    rect(width/2, 800, width/2, 50);
+    rect(width/2, 850, width/2, 50);
+    rect(width/2, 900, width/2, 50);
+    rect(width/2, 950, width/2, 50);
+  pop();
+  //places
+  push();
+  fill('#494740');
+  noStroke();
+  textSize(30);
+  textStyle(BOLD);
+  textAlign(CENTER, CENTER);
+    text('1', 222, 752);
+    text('2', 222, 802);
+    text('3', 222, 852);
+    text('4', 222, 902);
+    text('5', 222, 952);
+
+    for (i = 0; i < leaderboard.length; i++) {
+        text(leaderboard[i][0], 370, 752 + 50*i);
+        text(leaderboard[i][1], 533, 752 + 50*i);
+    }
+  pop();
+  //column titles
+  fill('#494740');
+  noStroke();
+  strokeWeight(4);
+  textSize(30);
+  textStyle(BOLD);
+  textAlign(CENTER, CENTER);
+    text('NAME', 370, 703);
+    text('SCORE', 533, 703);
+  pop();
 }
 
 function drawOpeningScreen() {
@@ -349,6 +515,7 @@ function addScore() {
     });
 
     hideNameDialog();
+    leaderboard = getScores();
 }
 
 function getScores() {
@@ -377,9 +544,9 @@ function hideNameDialog() {
 
 
 function keyPressed() {
-    //
-    //console.log("Key pressed: "  + keyCode);
-    player.keyPressHandler();
+    if (scene == 1 && explosion == undefined) {
+        player.keyPressHandler();
+    }
 }
 
 
